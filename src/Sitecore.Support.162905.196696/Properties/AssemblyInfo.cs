131b7a1be82d6e4674fb1362548a7030f4a76a34
DK\yho <yho@sitecore.net>
2017-12-04 16:19:30 +0800

    Commit patch

---
+++ src/Sitecore.Support.162905.196696/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.162905.196696")]
+[assembly: AssemblyProduct("Sitecore.Support.162905.196696")]
+[assembly: ComVisible(false)]
